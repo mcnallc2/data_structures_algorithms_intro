ddf967e41237232d019918cbf7e23d0db9385156
mcnallc2 <mcnallc2@tcd.ie>
2018-10-10 15:24:51 +0100

    finished version

--- assignment1/task1.c
+++ assignment1/task1.c
@@ -3,8 +3,8 @@
 #include <string.h>
 
 #define MAX_BUFFER 256 // Maximum string length this program can handle
-#define MAX_NAME_SIZE 50
-#define TABLE_SIZE 1500
+#define MAX_NAME_SIZE 256
+#define TABLE_SIZE 2000
 
 typedef struct Element{
 
@@ -14,15 +14,25 @@ typedef struct Element{
 }Element;
 
 int hash1(char *s);
-int next_field( FILE *f, char *buf, int max );
-void add_to_hashtable(char name[MAX_NAME_SIZE], Element *hash_table);
+void next_field( FILE *f, char *buf, int max );
+int add_to_hashtable(char *name, Element *hash_table);
 void print_hashtable(Element *hash_table);
+int count_hashtable(Element *hash_table);
+int search_table(char *find, Element *hash_table);
+
 
 int main ( int argc, char *argv[] ) {
 	FILE *f;		
 	char buffer[MAX_BUFFER];
+	char find[MAX_NAME_SIZE];
+	int collisions = 0;
 
     Element hash_table[TABLE_SIZE];
+
+	for(int i = 0; i<2000; i++){
+		strcpy(hash_table[i].Name, "");
+		hash_table[i].Frequency = 0;
+	}
 
 	// Users must pass the name of the input file through the command line. Make sure
 	// that we got an input file. If not, print a message telling the user how to use
@@ -31,26 +41,52 @@ int main ( int argc, char *argv[] ) {
 		printf("usage: csv FILE\n"); 
 		return EXIT_FAILURE; 
 	}
-
 	// Try to open the input file. If there is a problem, report failure and quit
 	f = fopen(argv[1], "r");
 	if(!f) { 
 		printf("unable to open %s\n", argv[1]); 
 		return EXIT_FAILURE; 
 	}
-
-	while( !next_field(f, buffer, MAX_BUFFER) ); // discard the header
-
 	// Now read and print records until the end of the file
 	while(!feof(f)) {
-		int eor = next_field( f, buffer, MAX_BUFFER );
-		//printf("%s%c\n", buffer, ((eor)? '\n':' '));  // double newline if eor is not 0
+		next_field(f, buffer, MAX_BUFFER);
+        if(add_to_hashtable(buffer, hash_table) == 1){
+			collisions++;
+		}
+	}
 
-        add_to_hashtable(buffer, hash_table);
-		print_hashtable(hash_table);
+	printf("names.csv loaded!!!\n\n");
+	printf("Number of terms = ");
+	printf("%d", count_hashtable(hash_table));
+	printf("\n");
+	printf("Collisions = ");
+	printf("%d", collisions);
+	printf("\n\n");
+	//print_hashtable(hash_table);
 
-		//printf("%d", key);
-		//printf("\n");
+
+	while(strcmp(find, "quit") != 0){
+
+		int found_key;
+		printf("Enter a term to get the frequency.\nType 'quit' to escape\n>> ");
+		scanf("%s", find);
+
+		if(strcmp(find, "quit") != 0){
+			found_key = search_table(find, hash_table);
+			if(found_key == 2001){
+				printf("\n\n'");
+				printf("%s", find);
+				printf("' is not in table");
+				printf("\n\n");
+			}
+			else{
+				printf("\n\n");
+				printf("%s", hash_table[found_key].Name);
+				printf(" - ");
+				printf("%d", hash_table[found_key].Frequency);
+				printf("\n\n");
+			}
+		}
 	}
 
 	// Always remember to close the file
@@ -58,51 +94,94 @@ int main ( int argc, char *argv[] ) {
 	return EXIT_SUCCESS;
 }
 
-void add_to_hashtable(char *name, Element *hash_table){
+int add_to_hashtable(char *name, Element *hash_table){
 
+	//find the key of the name
     int key = hash1(name);
-    strcpy(hash_table[key].Name, name);
-    hash_table[key].Frequency = 1;
+	int sentinel = 0;
+	int switc = 0;
+	int collision = 0;
+ 
 
+	//interate until the correct key is found
+	while(sentinel == 0){
+
+		//if this key is empty
+		if(hash_table[key].Frequency == 0){
+			strcpy(hash_table[key].Name, name);
+			hash_table[key].Frequency = 1;
+			sentinel = 1;
+			switc = 0;
+		}
+
+		//if it is the same exact same string increment frequency
+		else if(strcmp(name, hash_table[key].Name) == 0){
+			hash_table[key].Frequency = hash_table[key].Frequency + 1;
+			sentinel = 1;
+			switc = 0;
+		}
+		else if(switc == 0){
+			//printf("Collision\n");
+			collision = 1;
+			switc = 1;
+		}
+		//if the key is not empty but its only an anogram etc go to next key
+		key++;
+	}
+	return collision;
 }
 
 void print_hashtable(Element *hash_table){
 
-	for(int i = 0; i < 1501; i++){
+	for(int i = 0; i < 2000; i++){
 
-		printf("%s", hash_table[i].Name);
-		printf("\t");
-		printf("%d", hash_table[i].Frequency);
-		printf("\n");
-
+		if(hash_table[i].Frequency != 0){
+			
+			printf("%d", i);
+			printf("\t");
+			printf("%s", hash_table[i].Name);
+			printf("\t");
+			printf("%d", hash_table[i].Frequency);
+			printf("\n");
+		}
 	}
+}
 
+int count_hashtable(Element *hash_table){
 
+	int count = 0;
+	for(int i = 0; i < 2000; i++){
+		if(hash_table[i].Frequency != 0){
+			count++;
+		}
+	}
+	return count;
+}
+
+int search_table(char *find, Element *hash_table){
+
+	for(int i = 0; i < 2000; i++){
+
+		if(strcmp(find, hash_table[i].Name) == 0){
+			return i;
+		}
+	}
+	return 2001;
 }
 
 // The CSV parser
-int next_field( FILE *f, char *buf, int max ) {
+void next_field( FILE *f, char *buf, int max ) {
 
-	int i=0, end=0, quoted=0;
+	int i = 0;
 	
 	for(;;) {
 
 		// fetch the next character from file		
 		buf[i] = fgetc(f);
 
-		// if we encounter quotes then flip our state and immediately fetch next char
-		if(buf[i]=='"'){
-			quoted=!quoted; buf[i] = fgetc(f);
-		}
-
-		// end of field on comma if we're not inside quotes
-		if(buf[i]==',' && !quoted){
-			 break;
-		}
-
 		// end record on newline or end of file
 		if(feof(f) || buf[i]=='\n'){
-			 end=1; break;
+			break;
 		} 
 
 		// truncate fields that would overflow the buffer
@@ -112,7 +191,6 @@ int next_field( FILE *f, char *buf, int max ) {
 	}
 
 	buf[i] = 0; // null terminate the string
-	return end; // flag stating whether or not this is end of the line
 }
 
 int hash1(char *s){
